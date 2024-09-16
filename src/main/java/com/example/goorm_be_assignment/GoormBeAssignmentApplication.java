a4658aeae10bc3bbdfb831516df7e594ce1c0e8e
SongMinhyuk <dnfldpden32@gmail.com>
2024-09-16 18:18:57 +0900

    FEAT: 프로젝트 생성, 의존성 설정

---
+++ src/main/java/com/example/goorm_be_assignment/GoormBeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.goorm_be_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormBeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormBeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goorm_be_assignment/GoormBeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goorm_be_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormBeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
