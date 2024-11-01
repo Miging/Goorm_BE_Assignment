cc221fd3bb68264d0643af009968d3bac3892dff
SongMinhyuk <dnfldpden32@gmail.com>
2024-11-01 11:40:13 +0900

    chore: CourseService 서비스 어노테이션 추가

--- src/main/java/com/example/goorm_be_assignment/service/CourseService.java
+++ src/main/java/com/example/goorm_be_assignment/service/CourseService.java
@@ -1,14 +1,19 @@
 package com.example.goorm_be_assignment.service;
 
 import com.example.goorm_be_assignment.entity.Course;
+import com.example.goorm_be_assignment.repository.CourseRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Service
 public class CourseService {
+
     @Autowired
     private CourseRepository courseRepository;
+
     public List<Course> getCourses() {
         return courseRepository.findAll();
     }
